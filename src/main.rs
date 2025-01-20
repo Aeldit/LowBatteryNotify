212840cca86abf8170717bcd0e9ad726dc8fd2a7
aeldit <aeldit.66@proton.me>
2025-01-20 19:35:59 +0200

    The program is now working

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 extern crate battery;
 use std::process::Command;
 
-use std::io;
-use std::thread;
+use std::io::{Error, ErrorKind};
+use std::thread::sleep;
 use std::time::Duration;
 
-const CRATE_NAME: &str = std::env!("CARGO_PKG_NAME");
-const NOTIFICATION_TIMEOUT: u32 = 5000;
+use battery::State;
 
 fn main() -> battery::Result<()> {
     let manager = battery::Manager::new()?;
@@ -18,7 +17,7 @@ fn main() -> battery::Result<()> {
         }
         None => {
             eprintln!("Unable to find any batteries");
-            return Err(io::Error::from(io::ErrorKind::NotFound).into());
+            return Err(Error::from(ErrorKind::NotFound).into());
         }
     };
 
@@ -27,47 +26,40 @@ fn main() -> battery::Result<()> {
     let mut has_notified_10 = false;
     let mut has_notified_5 = false;
 
-    let mut pervious_percentage = (battery.state_of_charge().value * 100.0) as u8;
+    let mut cmd = Command::new("sh");
+    cmd.arg("-c").arg(
+        "notify-send -a \"LowBatteryNotify\" -u CRITICAL -t 5000 -p \"Battery Extremely Low (",
+    );
 
     loop {
         let percentage = (battery.state_of_charge().value * 100.0) as u8;
-        is_discharging = percentage <= pervious_percentage;
-        println!("{}", percentage);
-
-        if percentage <= 64 && !has_notified_5 {
-            has_notified_5 = is_discharging;
-            Command::new("sh")
-                .arg("-c")
-                .arg(format!(
-                    "notify-send -a \"{}\" -u CRITICAL -t {} -p \"Battery Extremely Low ({}%)\"",
-                    CRATE_NAME, NOTIFICATION_TIMEOUT, percentage
-                ))
-                .output()
-                .expect("Failed to run notify command");
-        } else if percentage <= 65 && !has_notified_10 {
-            has_notified_10 = is_discharging;
-            Command::new("sh")
-                .arg("-c")
-                .arg(format!(
-                    "notify-send -a \"{}\" -u CRITICAL -t {} -p \"Battery Quite Low ({}%)\"",
-                    CRATE_NAME, NOTIFICATION_TIMEOUT, percentage
-                ))
-                .output()
-                .expect("Failed to run notify command");
-        } else if percentage <= 66 && !has_notified_20 {
-            has_notified_20 = is_discharging;
-            Command::new("sh")
-                .arg("-c")
-                .arg(format!(
-                    "notify-send -a \"{}\" -u CRITICAL -t {} -p \"Battery Low ({}%)\"",
-                    CRATE_NAME, NOTIFICATION_TIMEOUT, percentage
-                ))
-                .output()
-                .expect("Failed to run notify command");
+        is_discharging = battery.state() == State::Discharging;
+        if !is_discharging {
+            has_notified_20 = false;
+            has_notified_10 = false;
+            has_notified_5 = false;
         }
 
-        thread::sleep(Duration::from_secs(20));
+        if is_discharging {
+            if percentage <= 5 && !has_notified_5 {
+                has_notified_5 = true;
+                cmd.arg(format!("{}%)\"", percentage))
+                    .output()
+                    .expect("Failed to run notify command");
+            } else if percentage <= 10 && !has_notified_10 {
+                has_notified_10 = true;
+                cmd.arg(format!("{}%)\"", percentage))
+                    .output()
+                    .expect("Failed to run notify command");
+            } else if percentage <= 20 && !has_notified_20 {
+                has_notified_20 = true;
+                cmd.arg(format!("{}%)\"", percentage))
+                    .output()
+                    .expect("Failed to run notify command");
+            }
+        }
+
+        sleep(Duration::from_secs(20));
         manager.refresh(&mut battery)?;
-        pervious_percentage = percentage;
     }
 }
